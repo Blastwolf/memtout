856b23becc90ee5fe48d5de35125f4c8f44ae814
BenjaminJ <Blastwolf@live.fr>
2019-06-07 14:22:28 +0200

    add keep awake fonction to server/main.js*

--- server/main.js
+++ server/main.js
@@ -1,8 +1,8 @@
 import '../imports/api/datas.js';
 import '../imports/startup/accounts_config.js';
-import {http} from 'meteor/http';
+import {HTTP} from 'meteor/http';
 
 setInterval(function(){
-    http.get('https://memtout.herokuapp.com');
+    HTTP.get('https://memtout.herokuapp.com');
     console.log('keep awake');
 },1800000);
